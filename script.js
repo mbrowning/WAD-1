4e8d10a02f94653451810348dd3958804cffad9f
Marshall Browning <marshall.browning@gmail.com>
2011-02-14 17:53:43 -0600

    finished design tweaks, commented out js on all but reg-1, commented out non-working js script, added alert script

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() 
+
+		{
+			alert('Robot dogs are awesome.');
+			});
+
+
+		/*{ var availableTags = [
+			"Fido",
+			"Spot",
+			"Bailey",
+			"Buddy",
+			"Ace",
+			"Bandit",
+			"Sport",
+			"Dodger",
+			"Bouncer",
+			"Genius",
+			"Fang",
+			"Lucky",
+			"Jester",
+			"Joker",
+			"Max",
+			"Rex",
+			"Angel",
+			"Baby",
+			"Deedee",
+			"Fifi",
+			"Ginger",
+			"Jasmine",
+			"Princess",
+			"Queen",
+			"Sasha",
+			"Celia",
+			"Gidget",
+			"Mavis",
+			"Oreo",
+		];
+		$( "#tags" ).autocomplete({
+			source: availableTags
+		});
+	}); */
